a4ff6194df922655195d40070f56e275227f855c
mculjak11 <76884550+mculjak11@users.noreply.github.com>
2022-05-21 04:58:35 +0200

    Svi zahtjevi dodani

--- src/main/java/com/is/infsusdz/controllers/CarFindController.java
+++ src/main/java/com/is/infsusdz/controllers/CarFindController.java
@@ -1,14 +1,16 @@
 package com.is.infsusdz.controllers;
 
+import com.google.common.collect.Sets;
 import com.is.infsusdz.users.*;
+import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class CarFindController {
@@ -82,9 +84,6 @@ public class CarFindController {
     @GetMapping(path="/api/ads/all", produces = "application/json")
     public ResponseEntity getAllAds() {
         List<CarFindAd> carAd = carFindAdRepo.findAll();
-        for (CarFindAd car : carAd) {
-            System.out.println(car.getMake());
-        }
         if (carAd.isEmpty()) {
             return ResponseEntity.ok()
                     .contentType(MediaType.TEXT_PLAIN)
@@ -96,20 +95,177 @@ public class CarFindController {
         }
     }
 
-    @PostMapping(path="/api/ads", produces = "application/json")
-    public ResponseEntity getAdsFilter(@RequestBody String filter) {
-        List<String> fil = List.of(filter.split(":"));
-        Integer first = Integer.parseInt(fil.get(0));
-        Integer second = Integer.parseInt(fil.get(1));
-        List<CarFindAd> carAd = carFindAdRepo.findCarFindAdByYearBetween(first, second);
-        if (carAd.isEmpty()) {
-            return ResponseEntity.ok()
-                    .contentType(MediaType.TEXT_PLAIN)
-                    .body("There are no ads with that filter");
-        } else {
+    @GetMapping(path="/api/ads/id={id}", produces = "application/json")
+    public ResponseEntity getAdById(@PathVariable String id) {
+        CarFindAd adById = carFindAdRepo.findCarFindAdById(id);
+        if (adById != null) {
             return ResponseEntity.ok()
                     .contentType(MediaType.APPLICATION_JSON)
-                    .body(carAd);
+                    .body(adById);
+        } else {
+            return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping(path="/api/ads/title={title}")
+    public ResponseEntity getAdByTitle(@PathVariable String title) {
+        List<CarFindAd> adByTitle = carFindAdRepo.findAll();
+        List<CarFindAd> adsMatch = new ArrayList<>();
+        for (CarFindAd ad : adByTitle) {
+            if (ad.getTitle().toUpperCase().contains(title.toUpperCase())){
+                adsMatch.add(ad);
+            }
+        }
+        if (!adsMatch.isEmpty()) {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(adsMatch);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @PostMapping(path="/api/ads/filter", produces = "application/json")
+    public ResponseEntity getAdsFilter(@RequestBody FilteredAd filter) {
+        List<List<String>> listAds = new ArrayList<>();
+        List<CarFindAd> allAds = carFindAdRepo.findAll();
+        List<String> allIds = new ArrayList<>();
+        for (CarFindAd ad: allAds) {
+            allIds.add(ad.getId());
+        }
+        List<CarFindAd> tmpAds = new ArrayList<>();
+        List<String> tmpIds = new ArrayList<>();
+
+        if (filter.getMake() != null) {
+            tmpAds = carFindAdRepo.findCarFindAdByMake(filter.getMake());
+            for (CarFindAd ad: tmpAds) {
+                tmpIds.add(ad.getId());
+            }
+            listAds.add(tmpIds);
+            tmpIds = new ArrayList<>();
+        } else listAds.add(allIds);
+
+        if (filter.getAdType() != null) {
+            tmpAds = carFindAdRepo.findCarFindAdByAdType(filter.getAdType());
+            for (CarFindAd ad: tmpAds) {
+                tmpIds.add(ad.getId());
+            }
+            listAds.add(tmpIds);
+            tmpIds = new ArrayList<>();
+        } else listAds.add(allIds);
+        if (filter.getCategory() != null) {
+            tmpAds = carFindAdRepo.findCarFindAdByCategory(filter.getCategory());
+            for (CarFindAd ad: tmpAds) {
+                tmpIds.add(ad.getId());
+            }
+            listAds.add(tmpIds);
+            tmpIds = new ArrayList<>();
+        } else listAds.add(allIds);
+        if (filter.getCity() != null) {
+            tmpAds = carFindAdRepo.findCarFindAdByCity(filter.getCity());
+            for (CarFindAd ad: tmpAds) {
+                tmpIds.add(ad.getId());
+            }
+            listAds.add(tmpIds);
+            tmpIds = new ArrayList<>();
+        } else listAds.add(allIds);
+        if (filter.getFuel() != null) {
+            tmpAds = carFindAdRepo.findCarFindAdByFuel(filter.getFuel());
+            for (CarFindAd ad: tmpAds) {
+                tmpIds.add(ad.getId());
+            }
+            listAds.add(tmpIds);
+            tmpIds = new ArrayList<>();
+        } else listAds.add(allIds);
+        if (filter.getModel() != null) {
+            tmpAds = carFindAdRepo.findCarFindAdByModel(filter.getModel());
+            for (CarFindAd ad: tmpAds) {
+                tmpIds.add(ad.getId());
+            }
+            listAds.add(tmpIds);
+            tmpIds = new ArrayList<>();
+        } else listAds.add(allIds);
+        if (filter.getShifter() != null) {
+            tmpAds = carFindAdRepo.findCarFindAdByShifter(filter.getShifter());
+            for (CarFindAd ad: tmpAds) {
+                tmpIds.add(ad.getId());
+            }
+            listAds.add(tmpIds);
+            tmpIds = new ArrayList<>();
+        } else listAds.add(allIds);
+
+        tmpAds = carFindAdRepo.findCarFindAdByYearIsBetween(filter.getYear1(), filter.getYear2());
+        for (CarFindAd ad: tmpAds) {
+            tmpIds.add(ad.getId());
+        }
+        listAds.add(tmpIds);
+
+        tmpIds = new ArrayList<>();
+        tmpAds = carFindAdRepo.findCarFindAdByPriceIsBetween(filter.getPrice1(), filter.getPrice2());
+        for (CarFindAd ad: tmpAds) {
+            tmpIds.add(ad.getId());
+        }
+        listAds.add(tmpIds);
+
+        tmpIds = new ArrayList<>();
+        tmpAds = carFindAdRepo.findCarFindAdByCm3IsBetween(filter.getCm31(), filter.getCm32());
+        for (CarFindAd ad: tmpAds) {
+            tmpIds.add(ad.getId());
+        }
+        listAds.add(tmpIds);
+
+        tmpIds = new ArrayList<>();
+        tmpAds = carFindAdRepo.findCarFindAdByKwIsBetween(filter.getKw1(), filter.getKw2());
+        for (CarFindAd ad: tmpAds) {
+            tmpIds.add(ad.getId());
+        }
+        listAds.add(tmpIds);
+
+        tmpIds = new ArrayList<>();
+        tmpAds = carFindAdRepo.findCarFindAdByKmIsBetween(filter.getKm1(), filter.getKm2());
+        for (CarFindAd ad: tmpAds) {
+            tmpIds.add(ad.getId());
+        }
+        listAds.add(tmpIds);
+        for (int i = 1; i < 12; i++) {
+            listAds.get(0).retainAll(listAds.get(i));
+        }
+        List<CarFindAd> finalAdList = new ArrayList<>();
+        CarFindAd finalAd = new CarFindAd();
+        for (String s: listAds.get(0)) {
+            finalAd = carFindAdRepo.findCarFindAdById(s);
+            finalAdList.add(finalAd);
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(finalAdList);
+    }
+
+    @PostMapping(path="/api/ads/action", produces = "application/json")
+    public ResponseEntity createAd(@RequestBody CarFindAd adCreate) {
+        carFindAdRepo.save(adCreate);
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body("Successfully created ad");
+    }
+
+    @PutMapping(path="/api/ads/action")
+    public ResponseEntity updateAd(@RequestBody CarFindAd adUpdate) {
+        CarFindAd ad = carFindAdRepo.findCarFindAdById(adUpdate.getId());
+        carFindAdRepo.delete(ad);
+        carFindAdRepo.save(adUpdate);
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body("Updated ad successfully");
+    }
+
+    @DeleteMapping(path="/api/ads/action")
+    public ResponseEntity deleteAd(@RequestBody String id) {
+        CarFindAd ad = carFindAdRepo.findCarFindAdById(id);
+        carFindAdRepo.delete(ad);
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body("Deleted ad successfully");
     }
 }
